ada290060b1021e73ae24917599e16dbaf4d8b28
Luka Vucina <lukavucina@Lukas-MacBook-Pro.local>
2021-01-14 19:58:03 +0100

    instructions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+             \URL::forceScheme('https');
+        }
     }
 }
